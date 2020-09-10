23fee43924d65f4c3666a19e8e661e277e2b0c48
Rohit Singh <conankmails@gmail.com>
2020-09-11 05:10:25 +0530

    picker/dropdown for ios/android added

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -88,7 +89,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.blue[700],
-            child:
+            child: Platform.isIOS ? getiOsPicker() : getAndroidDropdown(),
           ),
         ],
       ),
